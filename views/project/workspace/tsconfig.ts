44a1045718c2a9baeae8eab11773ba112a88ae6f
cplepage <lepage.charlesphilippe@gmail.com>
2025-10-27 08:37:45 -0400

    tsconfig moduleResolution bundler

--- views/project/workspace/tsconfig.ts
+++ views/project/workspace/tsconfig.ts
@@ -2,7 +2,7 @@ export const compilerOptions = {
     esModuleInterop: true,
     module: "es2022",
     target: "es2022",
-    moduleResolution: "node10",
+    moduleResolution: "bundler",
     allowJs: true,
     lib: ["dom", "dom.iterable", "es2023"],
     jsx: "react",
